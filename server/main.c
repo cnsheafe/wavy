61a984b4239149642b0f3cd874348594d5c90536
Chris <cnsheafe@pm.me>
2020-04-18 12:30:11 -0600

    Adds compositor & outut globals

--- server/main.c
+++ server/main.c
@@ -6,16 +6,35 @@ struct wl_server
 {
   struct wl_display *wl_display;
   struct wl_registry *registry;
+  struct wl_compositor *compositor;
+  struct wl_output *wl_output;
 };
 
 void create_registry_resource(
-  struct wl_client *client,
-  void *data,
-  uint32_t version,
-  uint32_t id
-  )
+    struct wl_client *client,
+    void *data,
+    uint32_t version,
+    uint32_t id)
 {
   wl_resource_create(client, &wl_registry_interface, version, id);
+}
+
+void create_compositor_resource(
+    struct wl_client *client,
+    void *data,
+    uint32_t version,
+    uint32_t id)
+{
+  wl_resource_create(client, &wl_compositor_interface, version, id);
+}
+
+void create_output_resource(
+    struct wl_client *client,
+    void *data,
+    uint32_t version,
+    uint32_t id)
+{
+  wl_resource_create(client, &wl_output_interface, version, id);
 }
 
 int main(int argc, char const *argv[])
@@ -44,6 +63,21 @@ int main(int argc, char const *argv[])
       wl_registry_interface.version,
       &server->registry,
       create_registry_resource);
+
+  // Adds the global compositor to the display
+  wl_global_create(
+      server->wl_display,
+      &wl_compositor_interface,
+      wl_compositor_interface.version,
+      &server->compositor,
+      create_compositor_resource);
+
+  wl_global_create(
+      server->wl_display,
+      &wl_output_interface,
+      wl_output_interface.version,
+      &server->wl_output,
+      create_output_resource);
 
   printf("Running Wayland display on %s\n", socket);
 
